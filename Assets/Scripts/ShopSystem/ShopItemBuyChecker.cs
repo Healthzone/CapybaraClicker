619e48bf254195c2e024cad1df124a7c172efa5f
Healthzone <emelyanov.valek@gmail.com>
2024-02-10 13:59:10 +0300

    Small changes

--- Assets/Scripts/ShopSystem/ShopItemBuyChecker.cs
+++ Assets/Scripts/ShopSystem/ShopItemBuyChecker.cs
@@ -1,8 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using YG;
 
 public class ShopItemBuyChecker : MonoBehaviour
 {
--- Assets/Scripts/ShopSystem/ShopItemDataBase.cs
+++ Assets/Scripts/ShopSystem/ShopItemDataBase.cs
@@ -6,27 +6,43 @@ namespace Assets.Scripts.ShopSystem
     {
         public string ClickCost = "";
         public int ClickLevel = 1;
+        public bool isOpenedClick = false;
 
         public string Click2Cost = "";
         public int Click2Level = 1;
+        public bool isOpenedClick2 = false;
 
         public string Click3Cost = "";
         public int Click3Level = 1;
+        public bool isOpenedClick3 = false;
 
         public string Click4Cost = "";
         public int Click4Level = 1;
+        public bool isOpenedClick4 = false;
+
+        public string Click5Cost = "";
+        public int Click5Level = 1;
+        public bool isOpenedClick5 = false;
 
         public string AutoClickCost = "";
         public int AutoClickLevel = 1;
+        public bool isOpenedAutoClick = false;
 
         public string AutoClick2Cost = "";
         public int AutoClick2Level = 1;
+        public bool isOpenedAutoClick2 = false;
 
         public string AutoClick3Cost = "";
         public int AutoClick3Level = 1;
+        public bool isOpenedAutoClick3 = false;
 
         public string AutoClick4Cost = "";
         public int AutoClick4Level = 1;
+        public bool isOpenedAutoClick4 = false;
+
+        public string AutoClick5Cost = "";
+        public int AutoClick5Level = 1;
+        public bool isOpenedAutoClick5 = false;
 
         public override string ToString()
         {
